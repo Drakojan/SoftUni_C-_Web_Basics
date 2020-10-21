4772f8a5a3c1790d55caea681d419e668d9746af
Nikolay Mitrev <nikolay.mitrev1@gmail.com>
2020-10-21 15:14:15 +0300

    upload Life Exam copy

--- exercise_2 - Asynchronous programming/SandBox/Program.cs
+++ exercise_2 - Asynchronous programming/SandBox/Program.cs
@@ -8,29 +8,29 @@ namespace SandBox
 {
     class Program
     {
-        static async Task Main(string[] args)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
+        //static async Task Main(string[] args)
+        //{
+        //    var sw = new Stopwatch();
+        //    sw.Start();
 
-            Console.WriteLine("Start of Program");
-                        
-            var result = Task.Run(async () =>
-            {
-                await Task.Delay(5000);
-                return "task 1 is completed";
-            });
+        //    Console.WriteLine("Start of Program");
 
-            for (int i = 0; i < 3; i++)
-            {
-                await Task.Delay(1000);
-                Console.WriteLine("working on task 1");
-            }
-           
-            Console.WriteLine(await result);
-            // Console.WriteLine(result.Result);
+        //    var result = Task.Run(async () =>
+        //    {
+        //        await Task.Delay(5000);
+        //        return "task 1 is completed";
+        //    });
 
-            Console.WriteLine($"End of program -{sw.Elapsed}");
-        }
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        await Task.Delay(1000);
+        //        Console.WriteLine("working on task 1");
+        //    }
+
+        //    Console.WriteLine(await result);
+        //    // Console.WriteLine(result.Result);
+
+        //    Console.WriteLine($"End of program -{sw.Elapsed}");
+        //}
     }
 }
